041b9be8ad3e5ee57ca2c3d0924d4852b9d0bb32
Ezekiel Banda <bsc-com-10-19@unima.ac.mw>
2022-01-09 10:16:17 +0200

    setup for the malawi book drive project

---
+++ src/main/java/com/ezekmw/mbd/MalawiBookDriveApplication.java
@@ -0,0 +1,13 @@
+package com.ezekmw.mbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalawiBookDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalawiBookDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezekmw/mbd/MalawiBookDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezekmw.mbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalawiBookDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
