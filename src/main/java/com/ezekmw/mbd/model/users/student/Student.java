4f64515074bf915ec5654077ab50c5f771a43c57
Ezekiel Banda <bsc-com-10-19@unima.ac.mw>
2022-01-10 13:17:29 +0200

    added phone number attribute and annotation

--- src/main/java/com/ezekmw/mbd/model/users/student/Student.java
+++ src/main/java/com/ezekmw/mbd/model/users/student/Student.java
@@ -2,9 +2,7 @@ package com.ezekmw.mbd.model.users.student;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 @Data
 @Entity
@@ -14,20 +12,23 @@ import java.time.LocalDate;
 @Setter
 @Getter
 public class Student {
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private @Id Long id;
     private String firstName;
     private String lastName;
     private String gender;
     private LocalDate dob;
     private String emailAddress;
+    private String phoneNumber;
     private String password;
 
-    public Student(String firstName, String lastName, String gender, LocalDate dob, String emailAddress, String password) {
+    public Student(String firstName, String lastName, String gender, LocalDate dob, String emailAddress,String phoneNumber, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
         this.dob = dob;
         this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
         this.password = password;
     }
 }
