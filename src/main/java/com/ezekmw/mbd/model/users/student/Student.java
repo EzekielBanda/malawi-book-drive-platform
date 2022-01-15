a4437cdbcac53c2bed551a1d5a7850edae176484
Ezekiel Banda <bsc-com-10-19@unima.ac.mw>
2022-01-15 23:17:15 +0200

    created relationship between entities

--- src/main/java/com/ezekmw/mbd/model/users/student/Student.java
+++ src/main/java/com/ezekmw/mbd/model/users/student/Student.java
@@ -1,28 +1,54 @@
 package com.ezekmw.mbd.model.users.student;
 
+import com.ezekmw.mbd.model.book.Book;
+import com.ezekmw.mbd.model.classNumber.ClassName;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
 @Data
-@Entity
-@Table
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
+@EqualsAndHashCode
+@Entity
+@Table(name = "student")
 public class Student {
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
     private @Id Long id;
+    @Column(name = "first_name",nullable = false)
     private String firstName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+    @Column(name = "gender",nullable = false)
     private String gender;
+    @Column(name = "date_of_birth",nullable = false)
     private LocalDate dob;
+    @Column(name = "email",nullable = false)
     private String emailAddress;
+    @Column(name = "phone_number")
     private String phoneNumber;
+    @Column(name = "password")
     private String password;
 
-    public Student(String firstName, String lastName, String gender, LocalDate dob, String emailAddress,String phoneNumber, String password) {
+
+    @ManyToOne
+    @JoinColumn(name = "class_name_id")
+    private ClassName className;
+
+    public Student(String firstName, String lastName, String gender, LocalDate dob, String emailAddress, String phoneNumber, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
@@ -30,5 +56,14 @@ public class Student {
         this.emailAddress = emailAddress;
         this.phoneNumber = phoneNumber;
         this.password = password;
+
     }
+    public ClassName getClassName() {
+        return className;
+    }
+
+    public void setClassName(ClassName className) {
+        this.className = className;
+    }
+
 }
